401f9dc0a009ee6d6b7eaf1ee399bee0c3e635b5
Benjamin Duffield <bduffield@palantir.com>
2016-05-23 22:27:06 +0200

    var/tmp/files -> var/data/tmp

--- src/main/groovy/com/palantir/gradle/javadist/DistributionExtension.groovy
+++ src/main/groovy/com/palantir/gradle/javadist/DistributionExtension.groovy
@@ -19,7 +19,7 @@ class DistributionExtension {
 
     private static final List<String> requiredJvmOpts = [
         '-Djava.security.egd=file:/dev/./urandom',
-        '-Djava.io.tmpdir=var/tmp/files'
+        '-Djava.io.tmpdir=var/data/tmp'
     ]
 
     private String serviceName
