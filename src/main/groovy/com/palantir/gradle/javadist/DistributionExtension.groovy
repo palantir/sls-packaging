7913d55e179bce18197ab61c3998e2f30492856e
Robert Fink <rfink@palantir.com>
2016-07-15 11:54:53 -0700

    Add link

--- src/main/groovy/com/palantir/gradle/javadist/DistributionExtension.groovy
+++ src/main/groovy/com/palantir/gradle/javadist/DistributionExtension.groovy
@@ -20,7 +20,7 @@ class DistributionExtension {
     private static final List<String> requiredJvmOpts = [
         '-Djava.security.egd=file:/dev/./urandom',
         '-Djava.io.tmpdir=var/data/tmp',
-        '-XX:+PerfDisableSharedMem'
+        '-XX:+PerfDisableSharedMem' // avoid memory-mapped IO during GC: http://www.evanjones.ca/jvm-mmap-pause.html
     ]
 
     private String serviceName
