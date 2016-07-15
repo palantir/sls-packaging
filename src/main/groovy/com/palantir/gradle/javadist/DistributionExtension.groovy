70194f64a199df66a9914bff1aa7b2387d05241d
Robert Fink <rfink@palantir.com>
2016-07-15 11:24:57 -0700

    Add -XX:+PerfDisableSharedMem flag by default
    This change improves performance by avoiding GC pauses when writing stats to
    a memory-mapped file under /tmp: http://www.evanjones.ca/jvm-mmap-pause.html.
    
    Note that this behaviour can be overridden by users via '-XX:-PerfDisableSharedMem'.

--- src/main/groovy/com/palantir/gradle/javadist/DistributionExtension.groovy
+++ src/main/groovy/com/palantir/gradle/javadist/DistributionExtension.groovy
@@ -19,7 +19,8 @@ class DistributionExtension {
 
     private static final List<String> requiredJvmOpts = [
         '-Djava.security.egd=file:/dev/./urandom',
-        '-Djava.io.tmpdir=var/data/tmp'
+        '-Djava.io.tmpdir=var/data/tmp',
+        '-XX:+PerfDisableSharedMem'
     ]
 
     private String serviceName
