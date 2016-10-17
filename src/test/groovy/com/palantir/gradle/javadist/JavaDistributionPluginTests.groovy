54e876f91a39ccbdd1cc3ad2ebd3b0b6789cbf04
Robert Fink <rfink@palantir.com>
2016-10-17 18:28:39 -0400

    Add default garabage collection JVM opts to launcherStatic, fixes #126

--- src/test/groovy/com/palantir/gradle/javadist/JavaDistributionPluginTests.groovy
+++ src/test/groovy/com/palantir/gradle/javadist/JavaDistributionPluginTests.groovy
@@ -259,7 +259,7 @@ class JavaDistributionPluginTests extends GradleTestSpec {
 
         then:
         String startScript = file('dist/service-name-0.1/service/bin/service-name', projectDir).text
-        startScript.contains('DEFAULT_JVM_OPTS=\'"-Djava.security.egd=file:/dev/./urandom" "-Djava.io.tmpdir=var/data/tmp" "-Xmx4M" "-Djavax.net.ssl.trustStore=truststore.jks"\'')
+        startScript.contains('DEFAULT_JVM_OPTS=\'"-Djava.security.egd=file:/dev/./urandom" "-Djava.io.tmpdir=var/data/tmp" "-XX:+PrintGCDateStamps" "-XX:+PrintGCDetails" "-XX:-TraceClassUnloading" "-XX:+UseGCLogFileRotation" "-XX:GCLogFileSize=10M" "-XX:NumberOfGCLogFiles=10" "-Xloggc:var/log/gc.log" "-verbose:gc" "-Xmx4M" "-Djavax.net.ssl.trustStore=truststore.jks"\'')
     }
 
     def 'produce distribution bundle that populates launcher-static.yml and launcher-check.yml'() {
@@ -289,6 +289,14 @@ class JavaDistributionPluginTests extends GradleTestSpec {
         expectedStaticConfig.setJvmOpts([
                 '-Djava.security.egd=file:/dev/./urandom',
                 '-Djava.io.tmpdir=var/data/tmp',
+                '-XX:+PrintGCDateStamps',
+                '-XX:+PrintGCDetails',
+                '-XX:-TraceClassUnloading',
+                '-XX:+UseGCLogFileRotation',
+                '-XX:GCLogFileSize=10M',
+                '-XX:NumberOfGCLogFiles=10',
+                '-Xloggc:var/log/gc.log',
+                '-verbose:gc',
                 '-Xmx4M',
                 '-Djavax.net.ssl.trustStore=truststore.jks'])
         def actualStaticConfig = new ObjectMapper(new YAMLFactory()).readValue(
