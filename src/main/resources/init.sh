9fc874f04375a73df31a8c0ecd9f929375bb1bf8
Huw Pryce <hpryce@palantir.com>
2016-11-30 14:02:09 +0000

    Return 2 on check failure.
    Dropwizard doesn't allow for setting the return value and Nagios monitoring
    expects a 2 for critical failure or 1 for a warning and healthcheck failures
    shouldn't be warnings.

--- src/main/resources/init.sh
+++ src/main/resources/init.sh
@@ -138,13 +138,12 @@ restart)
 check)
     printf "%-50s" "Checking health of '$SERVICE'..."
     $LAUNCHER_CMD $STATIC_LAUNCHER_CHECK_CONFIG > var/log/$SERVICE-check.log 2>&1
-    RESULT=$?
     if [ $RESULT -eq 0 ]; then
         printf "%s\n" "Healthy"
         exit 0
     else
         printf "%s\n" "Unhealthy"
-        exit $RESULT
+        exit 2
     fi
 ;;
 *)
