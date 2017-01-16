3044524116aa9079cae4f02a109211daaee79eda
Div Arora <darora@palantir.com>
2017-01-16 12:22:51 -0500

    Refactor existing code and add support for asset generation.

---
+++ src/main/groovy/com/palantir/gradle/dist/asset/AssetDistributionExtension.groovy
@@ -0,0 +1,26 @@
+package com.palantir.gradle.dist.asset
+
+import com.palantir.gradle.dist.BaseDistributionExtension
+import org.gradle.api.Project
+
+class AssetDistributionExtension extends BaseDistributionExtension {
+
+    private Map<String, String> assetsDirs = [:]
+
+    AssetDistributionExtension(Project project) {
+        super(project)
+        setProductType("asset.v1")
+    }
+
+    public Map<String, String> getAssetsDirs() {
+        return assetsDirs
+    }
+
+    public void assetsDir(String relativeSourcePath, String relativeDestinationPath) {
+        this.assetsDirs.put(relativeSourcePath, relativeDestinationPath)
+    }
+
+    public void setAssetsDirs(Map<String, String> assets) {
+        this.assetsDirs = assets
+    }
+}
