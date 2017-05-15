78f3048f56b0a9de582ed8c470f85d49d1075466
Nicholas Gates <ngates@palantir.com>
2017-05-15 17:33:45 +0100

    Resolve paths so absolute paths are respected

--- gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/asset/tasks/AssetDistTarTask.groovy
+++ gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/asset/tasks/AssetDistTarTask.groovy
@@ -36,7 +36,7 @@ class AssetDistTarTask {
             }
 
             assetDirs.entrySet().each { entry ->
-                from("${project.projectDir}/${entry.getKey()}") {
+                from(file(project.projectDir, entry.getKey())) {
                     into("${archiveRootDir}/asset/${entry.getValue()}")
                 }
             }
