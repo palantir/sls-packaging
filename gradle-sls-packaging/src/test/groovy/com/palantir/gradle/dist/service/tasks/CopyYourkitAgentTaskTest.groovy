ebc65b9905a9f3ea5170fef55b6f84d13ea60dc8
Jeffrey Nelson <jeffn@palantir.com>
2018-03-29 13:54:05 -0700

    add test to check that dist has valid yjp so file

--- gradle-sls-packaging/src/test/groovy/com/palantir/gradle/dist/service/tasks/CopyYourkitAgentTaskTest.groovy
+++ gradle-sls-packaging/src/test/groovy/com/palantir/gradle/dist/service/tasks/CopyYourkitAgentTaskTest.groovy
@@ -8,7 +8,7 @@ class CopyYourkitAgentTaskTest extends GradleTestSpec {
 
     def 'copyYourkitAgent task is up to date if already run'() {
         setup:
-        createBuildFile(buildFile, "java-service", "foo-service")
+        createUntarBuildFile(buildFile)
 
         when:
         BuildResult buildResult = run(':copyYourkitAgent').build()
@@ -23,18 +23,51 @@ class CopyYourkitAgentTaskTest extends GradleTestSpec {
         buildResult.task(':copyYourkitAgent').outcome == TaskOutcome.UP_TO_DATE
     }
 
-    private static createBuildFile(buildFile, pluginType, name) {
-        buildFile << """
+    def 'Build produces libyjpagent file'() {
+        given:
+        createUntarBuildFile(buildFile)
+
+        when:
+        runSuccessfully(':build', ':distTar', ':untar')
+
+        then:
+        file('dist/service-name-0.0.1').exists()
+        file('dist/service-name-0.0.1/service/lib/linux-x86-64/libyjpagent.so').exists()
+        file('dist/service-name-0.0.1/service/lib/linux-x86-64/libyjpagent.so').getBytes().length > 0
+    }
+
+    protected runSuccessfully(String... tasks) {
+        BuildResult buildResult = run(tasks).build()
+        tasks.each { buildResult.task(it).outcome == TaskOutcome.SUCCESS }
+        return buildResult
+    }
+
+    private static createUntarBuildFile(buildFile) {
+        buildFile << '''
             plugins {
-                id 'com.palantir.sls-${pluginType}-distribution'
+                id 'com.palantir.sls-java-service-distribution'
+                id 'java'
             }
+
+            project.group = 'service-group'
+
+            repositories { jcenter() }
+
+            version '0.0.1'
 
             distribution {
-                serviceName '${name}'
+                serviceName 'service-name'
+                mainClass 'test.Test'
             }
 
-            version "0.0.1"
-            project.group = 'service-group'
-        """.stripIndent()
+            sourceCompatibility = '1.7'
+
+            // most convenient way to untar the dist is to use gradle
+            task untar (type: Copy) {
+                from tarTree(resources.gzip("${buildDir}/distributions/service-name-0.0.1.sls.tgz"))
+                into "${projectDir}/dist"
+                dependsOn distTar
+            }
+        '''.stripIndent()
     }
 }
