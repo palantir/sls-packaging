49ee3fad3f5f76b060dcf412718a938f9ca98853
Dan Sanduleac <dsanduleac@palantir.com>
2018-06-13 13:04:08 -0400

    correct format and more copyrights

--- gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/RecommendedProductDependencyCombiner.groovy
+++ gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/RecommendedProductDependencyCombiner.groovy
@@ -13,27 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.palantir.gradle.dist.service.util
 
-import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Path
-import java.util.Map.Entry
+package com.palantir.gradle.dist
 
-class EmitFiles {
-
-    static Path replaceVars(InputStream src, Path dest, Map<String, String> vars) {
-        String text = new String(src.getText())
-
-        for (Entry<String, String> entry : vars) {
-            text = text.replaceAll(entry.key, entry.value)
-        }
-
-        // ensure output directory exists
-        dest.getParent().toFile().mkdirs()
-
-        // write content
-        return Files.write(dest, text.getBytes(Charset.forName("UTF-8")))
-    }
-
+class RecommendedProductDependencyCombiner {
 }
