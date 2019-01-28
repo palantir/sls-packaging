c415a5b20befea6383ec3f793824ea9a72a37455
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2019-01-28 12:10:41 +0000

    [fix] Restore old setter methods (without set*) and add lazy setters (#393)
    ## Before this PR
    
    Rewrite in #385 removed some setter methods e.g. `JavaServiceDistributionExtension#mainClass(String)`.
    
    ## After this PR
    
    Re-add old setter methods.
    
    <!-- Reference any existing GitHub issues, e.g. 'fixes #000' or 'relevant to #000' -->

--- gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/service/JavaServiceDistributionExtension.java
+++ gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/service/JavaServiceDistributionExtension.java
@@ -27,6 +27,7 @@ import com.palantir.gradle.dist.service.gc.ResponseTime;
 import com.palantir.gradle.dist.service.gc.ResponseTime11;
 import com.palantir.gradle.dist.service.gc.Throughput;
 import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -66,7 +67,7 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
     public JavaServiceDistributionExtension(Project project) {
         super(project);
 
-        this.objectFactory = project.getObjects();
+        objectFactory = project.getObjects();
         mainClass = objectFactory.property(String.class);
         javaHome = objectFactory.property(String.class);
         addJava8GcLogging = objectFactory.property(Boolean.class);
@@ -89,16 +90,16 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
         return mainClass;
     }
 
-    public final void setMainClass(String mainClass) {
-        this.mainClass.set(mainClass);
+    public final void mainClass(String newMainClass) {
+        this.mainClass.set(newMainClass);
     }
 
     public final Provider<String> getJavaHome() {
         return javaHome;
     }
 
-    public final void setJavaHome(String javaHome) {
-        this.javaHome.set(javaHome);
+    public final void javaHome(String newJavaHome) {
+        this.javaHome.set(newJavaHome);
     }
 
     public final Provider<Boolean> getAddJava8GcLogging() {
@@ -109,12 +110,16 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
         this.addJava8GcLogging.set(addJava8GcLogging);
     }
 
+    public final void addJava8GcLogging(boolean newAddJava8GcLogging) {
+        this.addJava8GcLogging.set(newAddJava8GcLogging);
+    }
+
     public final Provider<Boolean> getEnableManifestClasspath() {
         return enableManifestClasspath;
     }
 
-    public final void setEnableManifestClasspath(boolean enableManifestClasspath) {
-        this.enableManifestClasspath.set(enableManifestClasspath);
+    public final void enableManifestClasspath(boolean newEnableManifestClasspath) {
+        this.enableManifestClasspath.set(newEnableManifestClasspath);
     }
 
     public final Provider<List<String>> getArgs() {
@@ -122,6 +127,10 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
     }
 
     public final void args(String... newArgs) {
+        this.args.addAll(newArgs);
+    }
+
+    public final void args(Provider<Iterable<String>> newArgs) {
         this.args.addAll(newArgs);
     }
 
@@ -137,6 +146,10 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
         this.checkArgs.addAll(newCheckArgs);
     }
 
+    public final void checkArgs(Provider<Iterable<String>> newCheckArgs) {
+        this.checkArgs.addAll(newCheckArgs);
+    }
+
     public final void setCheckArgs(Iterable<String> checkArgs) {
         this.checkArgs.set(checkArgs);
     }
@@ -149,6 +162,10 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
         this.defaultJvmOpts.addAll(opts);
     }
 
+    public final void defaultJvmOpts(Provider<Iterable<String>> opts) {
+        this.defaultJvmOpts.addAll(opts);
+    }
+
     public final void setDefaultJvmOpts(Iterable<String> defaultJvmOpts) {
         this.defaultJvmOpts.set(defaultJvmOpts);
     }
@@ -158,6 +175,10 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
     }
 
     public final void excludeFromVar(String... fromVar) {
+        this.excludeFromVar.addAll(fromVar);
+    }
+
+    public final void excludeFromVar(Provider<Iterable<String>> fromVar) {
         this.excludeFromVar.addAll(fromVar);
     }
 
@@ -181,7 +202,7 @@ public class JavaServiceDistributionExtension extends BaseDistributionExtension 
         return gc;
     }
 
-    public final void gc(String type, @Nullable Closure configuration) {
+    public final void gc(String type, @Nullable @DelegatesTo(GcProfile.class) Closure configuration) {
         GcProfile newGc = objectFactory.newInstance(profileNames.get(type));
         if (configuration != null) {
             ConfigureUtil.configure(configuration, newGc);
