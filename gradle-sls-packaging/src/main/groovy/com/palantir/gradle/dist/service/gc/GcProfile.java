9073d2dd70c96b776b1f8a18ab2d051e9693752f
Dan Fox <dfox@palantir.com>
2019-02-11 10:47:03 +0000

    Pure refactor: move all GcProfile stuff to one file

--- gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/service/gc/GcProfile.java
+++ gradle-sls-packaging/src/main/groovy/com/palantir/gradle/dist/service/gc/GcProfile.java
@@ -16,12 +16,72 @@
 
 package com.palantir.gradle.dist.service.gc;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 public interface GcProfile extends Serializable {
     long serialVersionUID = 1L;
 
+    @VisibleForTesting
+    Map<String, Class<? extends GcProfile>> PROFILE_NAMES = ImmutableMap.of(
+            "throughput", GcProfile.Throughput.class,
+            "response-time", GcProfile.ResponseTime.class,
+            "hybrid", GcProfile.Hybrid.class,
+            "response-time-11", GcProfile.ResponseTime11.class);
+
     List<String> gcJvmOpts();
 
+    class Throughput implements GcProfile {
+        @Override
+        public final List<String> gcJvmOpts() {
+            return ImmutableList.of("-XX:+UseParallelOldGC");
+        }
+    }
+
+    class ResponseTime implements GcProfile {
+        private int initiatingOccupancyFraction = 68;
+
+        @Override
+        public final List<String> gcJvmOpts() {
+            return ImmutableList.of("-XX:+UseParNewGC",
+                    "-XX:+UseConcMarkSweepGC",
+                    "-XX:+UseCMSInitiatingOccupancyOnly",
+                    "-XX:CMSInitiatingOccupancyFraction=" + initiatingOccupancyFraction,
+                    "-XX:+CMSClassUnloadingEnabled",
+                    "-XX:+ExplicitGCInvokesConcurrent",
+                    "-XX:+ClassUnloadingWithConcurrentMark",
+                    "-XX:+CMSScavengeBeforeRemark",
+                    // 'UseParNewGC' was removed in Java10: https://bugs.openjdk.java.net/browse/JDK-8173421
+                    "-XX:+IgnoreUnrecognizedVMOptions");
+        }
+
+        public final void initiatingOccupancyFraction(int occupancyFraction) {
+            this.initiatingOccupancyFraction = occupancyFraction;
+        }
+    }
+
+    class Hybrid implements GcProfile {
+        @Override
+        public final List<String> gcJvmOpts() {
+            return ImmutableList.of(
+                    "-XX:+UseG1GC",
+                    "-XX:+UseStringDeduplication");
+        }
+    }
+
+    class ResponseTime11 implements GcProfile {
+        @Override
+        public final List<String> gcJvmOpts() {
+            return ImmutableList.of(
+                    // https://wiki.openjdk.java.net/display/shenandoah/Main
+                    "-XX:+UseShenandoahGC",
+                    // "forces concurrent cycle instead of Full GC on System.gc()"
+                    "-XX:+ExplicitGCInvokesConcurrent",
+                    "-XX:+ClassUnloadingWithConcurrentMark");
+        }
+    }
 }
