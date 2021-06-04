f2c52ddf26fd1eabd8f2478fd0470ccceed80105
jakubk <jakubk@palantir.com>
2021-06-04 09:32:32 +0100

    Bump go-java-launcher version.

--- gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/service/JavaServiceDistributionPlugin.java
+++ gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/service/JavaServiceDistributionPlugin.java
@@ -54,8 +54,9 @@ import org.gradle.util.GFileUtils;
 import org.gradle.util.GradleVersion;
 
 public final class JavaServiceDistributionPlugin implements Plugin<Project> {
-    private static final String GO_JAVA_LAUNCHER = "com.palantir.launching:go-java-launcher:1.11.0";
-    private static final String GO_INIT = "com.palantir.launching:go-init:1.11.0";
+    private static final String GO_JAVA_VERSION = "1.12.0-rc3";
+    private static final String GO_JAVA_LAUNCHER = "com.palantir.launching:go-java-launcher:" + GO_JAVA_VERSION;
+    private static final String GO_INIT = "com.palantir.launching:go-init:" + GO_JAVA_VERSION;
     public static final String GROUP_NAME = "Distribution";
 
     @Override
