3394bc9307cd81847651376013cf52ec702ee6c7
Eric Sword <esword@palantir.com>
2021-06-08 15:22:44 -0400

    Revert "resolve race condition with jar manifest manipulation (#1150)"
    This reverts commit 3e3cd17e

--- gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/tasks/ResolveProductDependenciesTask.java
+++ gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/tasks/ResolveProductDependenciesTask.java
@@ -20,23 +20,23 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.palantir.gradle.dist.BaseDistributionExtension;
+import com.palantir.gradle.dist.ConfigureProductDependenciesTask;
 import com.palantir.gradle.dist.ProductDependency;
 import com.palantir.gradle.dist.ProductDependencyMerger;
 import com.palantir.gradle.dist.ProductDependencyReport;
 import com.palantir.gradle.dist.ProductId;
 import com.palantir.gradle.dist.RecommendedProductDependencies;
+import com.palantir.gradle.dist.RecommendedProductDependenciesPlugin;
 import com.palantir.gradle.dist.Serializations;
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
-import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -45,11 +45,15 @@ import java.util.zip.ZipFile;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ResolvedArtifact;
-import org.gradle.api.artifacts.ResolvedDependency;
+import org.gradle.api.artifacts.component.ComponentIdentifier;
+import org.gradle.api.artifacts.component.ProjectComponentIdentifier;
+import org.gradle.api.artifacts.result.ResolvedComponentResult;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.Provider;
@@ -59,12 +63,17 @@ import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskProvider;
+import org.gradle.jvm.tasks.Jar;
 
 @CacheableTask
 public abstract class ResolveProductDependenciesTask extends DefaultTask {
     private static final Logger log = Logging.getLogger(ResolveProductDependenciesTask.class);
 
+    private final Property<Configuration> productDependenciesConfig =
+            getProject().getObjects().property(Configuration.class);
+
     public ResolveProductDependenciesTask() {
+        dependsOn(otherProjectProductDependenciesTasks());
         getOutputFile().convention(() -> new File(getTemporaryDir(), "resolved-product-dependencies.json"));
         getDiscoveredProductDependencies().convention(getProject().provider(this::findRecommendedProductDependenies));
     }
@@ -73,20 +82,46 @@ public abstract class ResolveProductDependenciesTask extends DefaultTask {
             Project project, BaseDistributionExtension ext, Provider<Set<ProductId>> provider) {
         TaskProvider<ResolveProductDependenciesTask> depTask = project.getTasks()
                 .register("resolveProductDependencies", ResolveProductDependenciesTask.class, task -> {
-                    Provider<Configuration> configProvider = project.provider(ext::getProductDependenciesConfig);
                     task.getServiceName().set(ext.getDistributionServiceName());
                     task.getServiceGroup().set(ext.getDistributionServiceGroup());
                     task.getDeclaredProductDependencies().set(ext.getAllProductDependencies());
-                    task.getProductDependenciesConfig().set(configProvider);
+                    task.setConfiguration(project.provider(ext::getProductDependenciesConfig));
                     task.getOptionalProductIds().set(ext.getOptionalProductDependencies());
                     task.getIgnoredProductIds().set(ext.getIgnoredProductDependencies());
                     task.getInRepoProductIds().set(provider);
-
-                    // Need the dependsOn because the configuration is not set as an Input property and
-                    // we need the artifacts in it to be resolved.
-                    task.dependsOn(configProvider);
                 });
         return depTask;
+    }
+
+    /**
+     * A lazy collection of tasks that ensure the {@link Jar} task of any project dependencies from
+     * {@link #productDependenciesConfig} is correctly populated with the recommended product dependencies of that
+     * project, if any (specifically, if they apply the {@link RecommendedProductDependenciesPlugin}).
+     */
+    private Provider<FileCollection> otherProjectProductDependenciesTasks() {
+        return productDependenciesConfig.map(productDeps -> {
+            // Using a ConfigurableFileCollection simply because it implements Buildable and provides a convenient API
+            // to wire up task dependencies to it in a lazy way.
+            ConfigurableFileCollection emptyFileCollection = getProject().files();
+            productDeps.getIncoming().getArtifacts().getArtifacts().stream()
+                    .flatMap(artifact -> {
+                        ComponentIdentifier id = artifact.getId().getComponentIdentifier();
+
+                        // Depend on the ConfigureProductDependenciesTask, if it exists, which will wire up the jar
+                        // manifest
+                        // with recommended product dependencies.
+                        if (id instanceof ProjectComponentIdentifier) {
+                            Project dependencyProject = getProject()
+                                    .getRootProject()
+                                    .project(((ProjectComponentIdentifier) id).getProjectPath());
+                            return Stream.of(
+                                    dependencyProject.getTasks().withType(ConfigureProductDependenciesTask.class));
+                        }
+                        return Stream.empty();
+                    })
+                    .forEach(emptyFileCollection::builtBy);
+            return emptyFileCollection;
+        });
     }
 
     @Input
@@ -110,8 +145,22 @@ public abstract class ResolveProductDependenciesTask extends DefaultTask {
     @Input
     public abstract SetProperty<ProductId> getInRepoProductIds();
 
+    /**
+     * Contents of the given configuration.  Cannot list the configuration itself as an input property because the
+     * caching calculations attempt to resolve the configuration at config time.  This can lead to an error for
+     * configs that connot be resolved directly at that stage.  Caching and up-to-date calcs thus use this property.
+     */
     @Input
-    public abstract Property<Configuration> getProductDependenciesConfig();
+    final Set<String> getProductDependenciesConfig() {
+        return productDependenciesConfig.get().getIncoming().getResolutionResult().getAllComponents().stream()
+                .map(ResolvedComponentResult::getId)
+                .map(ComponentIdentifier::getDisplayName)
+                .collect(Collectors.toSet());
+    }
+
+    final void setConfiguration(Provider<Configuration> config) {
+        this.productDependenciesConfig.set(config);
+    }
 
     @OutputFile
     public abstract RegularFileProperty getOutputFile();
@@ -205,58 +254,93 @@ public abstract class ResolveProductDependenciesTask extends DefaultTask {
     }
 
     private List<ProductDependency> findRecommendedProductDependenies() {
-        // This will find both intra-project and third party artifacts because the project artifacts are resolved to
-        // their generated jar files.
-        Stream<ResolvedArtifact> artifactStream =
-                getProductDependenciesConfig()
-                        .get()
-                        .getResolvedConfiguration()
-                        .getFirstLevelModuleDependencies()
-                        .stream()
-                        .map(ResolvedDependency::getAllModuleArtifacts)
-                        .flatMap(Collection::stream)
-                        .filter(a -> "jar".equals(a.getExtension()))
-                        .distinct();
+        return productDependenciesConfig.get().getIncoming().getArtifacts().getArtifacts().stream()
+                .flatMap(artifact -> {
+                    String artifactName = artifact.getId().getDisplayName();
+                    ComponentIdentifier id = artifact.getId().getComponentIdentifier();
+                    Optional<String> pdeps = Optional.empty();
 
-        return artifactStream
-                .map(ResolveProductDependenciesTask::getProductDependenciesFromArtifact)
-                .flatMap(Collection::stream)
-                .distinct()
+                    // Extract product dependencies directly from Jar task for in project dependencies
+                    if (id instanceof ProjectComponentIdentifier) {
+                        Project dependencyProject = getProject()
+                                .getRootProject()
+                                .project(((ProjectComponentIdentifier) id).getProjectPath());
+                        if (dependencyProject.getPlugins().hasPlugin(JavaPlugin.class)) {
+                            Jar jar = (Jar) dependencyProject.getTasks().getByName(JavaPlugin.JAR_TASK_NAME);
+
+                            pdeps = Optional.ofNullable(jar.getManifest()
+                                            .getEffectiveManifest()
+                                            .getAttributes()
+                                            .get(RecommendedProductDependencies.SLS_RECOMMENDED_PRODUCT_DEPS_KEY))
+                                    .map(Object::toString);
+                        }
+                    } else {
+                        if (!artifact.getFile().exists()) {
+                            log.debug("Artifact did not exist: {}", artifact.getFile());
+                            return Stream.empty();
+                        } else if (!com.google.common.io.Files.getFileExtension(
+                                        artifact.getFile().getName())
+                                .equals("jar")) {
+                            log.debug("Artifact is not jar: {}", artifact.getFile());
+                            return Stream.empty();
+                        }
+
+                        Manifest manifest;
+                        try {
+                            ZipFile zipFile = new ZipFile(artifact.getFile());
+                            ZipEntry manifestEntry = zipFile.getEntry("META-INF/MANIFEST.MF");
+                            if (manifestEntry == null) {
+                                log.debug("Manifest file does not exist in jar for '${coord}'");
+                                return Stream.empty();
+                            }
+                            manifest = new Manifest(zipFile.getInputStream(manifestEntry));
+                        } catch (IOException e) {
+                            log.warn(
+                                    "IOException encountered when processing artifact '{}', file '{}', {}",
+                                    artifactName,
+                                    artifact.getFile(),
+                                    e);
+                            return Stream.empty();
+                        }
+
+                        pdeps = Optional.ofNullable(manifest.getMainAttributes()
+                                .getValue(RecommendedProductDependencies.SLS_RECOMMENDED_PRODUCT_DEPS_KEY));
+                    }
+                    if (!pdeps.isPresent()) {
+                        log.debug(
+                                "No product dependency found for artifact '{}', file '{}'",
+                                artifactName,
+                                artifact.getFile());
+                        return Stream.empty();
+                    }
+
+                    try {
+                        RecommendedProductDependencies recommendedDeps =
+                                Serializations.jsonMapper.readValue(pdeps.get(), RecommendedProductDependencies.class);
+                        return recommendedDeps.recommendedProductDependencies().stream()
+                                .peek(productDependency -> log.info(
+                                        "Product dependency recommendation made by artifact '{}', file '{}', "
+                                                + "dependency recommendation '{}'",
+                                        artifactName,
+                                        artifact,
+                                        productDependency))
+                                .map(recommendedDep -> new ProductDependency(
+                                        recommendedDep.getProductGroup(),
+                                        recommendedDep.getProductName(),
+                                        recommendedDep.getMinimumVersion(),
+                                        recommendedDep.getMaximumVersion(),
+                                        recommendedDep.getRecommendedVersion(),
+                                        recommendedDep.getOptional()));
+                    } catch (IOException | IllegalArgumentException e) {
+                        log.debug(
+                                "Failed to load product dependency for artifact '{}', file '{}', '{}'",
+                                artifactName,
+                                artifact,
+                                e);
+                        return Stream.empty();
+                    }
+                })
                 .collect(Collectors.toList());
-    }
-
-    private static Collection<ProductDependency> getProductDependenciesFromArtifact(ResolvedArtifact artifact) {
-        File jarFile = artifact.getFile();
-        try (ZipFile zipFile = new ZipFile(jarFile)) {
-            String artifactName = artifact.getId().getDisplayName();
-            ZipEntry manifestEntry = zipFile.getEntry("META-INF/MANIFEST.MF");
-            if (manifestEntry == null) {
-                return Collections.emptySet();
-            }
-            Attributes attrs = new Manifest(zipFile.getInputStream(manifestEntry)).getMainAttributes();
-            if (!attrs.containsKey(RecommendedProductDependencies.SLS_RECOMMENDED_PRODUCT_DEPS_ATTRIBUTE)) {
-                return Collections.emptySet();
-            }
-
-            Set<ProductDependency> recommendedDeps = Serializations.jsonMapper
-                    .readValue(
-                            attrs.getValue(RecommendedProductDependencies.SLS_RECOMMENDED_PRODUCT_DEPS_ATTRIBUTE),
-                            RecommendedProductDependencies.class)
-                    .recommendedProductDependencies();
-            if (recommendedDeps.isEmpty()) {
-                return Collections.emptySet();
-            }
-            log.info(
-                    "Product dependency recommendation made by artifact '{}', file '{}', "
-                            + "dependency recommendation '{}'",
-                    artifactName,
-                    artifact,
-                    recommendedDeps);
-            return recommendedDeps;
-        } catch (IOException e) {
-            log.warn("Failed to load product dependency for artifact '{}', file '{}', '{}'", artifact, jarFile, e);
-            return Collections.emptySet();
-        }
     }
 
     private boolean isNotSelfProductDependency(ProductDependency dependency) {
