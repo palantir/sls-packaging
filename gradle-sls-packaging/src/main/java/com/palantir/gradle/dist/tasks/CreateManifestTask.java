461af01c631f37b89e5cb1add1149c92fa4d6032
forozco <forozco@palantir.com>
2021-06-15 12:53:30 -0400

    ugh

--- gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/tasks/CreateManifestTask.java
+++ gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/tasks/CreateManifestTask.java
@@ -61,6 +61,7 @@ import org.gradle.api.provider.SetProperty;
 import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskProvider;
 import org.gradle.language.base.plugins.LifecycleBasePlugin;
@@ -93,7 +94,7 @@ public abstract class CreateManifestTask extends DefaultTask {
     @Input
     abstract RegularFileProperty getProductDependenciesFile();
 
-    @InputFile
+    @OutputFile
     abstract RegularFileProperty getManifestFile();
 
     @Input
