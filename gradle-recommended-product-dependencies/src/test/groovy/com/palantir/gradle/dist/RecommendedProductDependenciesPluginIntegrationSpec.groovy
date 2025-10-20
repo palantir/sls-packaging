316708de275027e1dbf25a691029f95cd558a54b
Felix de Souza <fdesouza@palantir.com>
2025-10-20 15:54:14 +0100

    Bump GCV in `RecommendedProductDependenciesPluginIntegrationSpec` to version supporting Gradle 9

--- gradle-recommended-product-dependencies/src/test/groovy/com/palantir/gradle/dist/RecommendedProductDependenciesPluginIntegrationSpec.groovy
+++ gradle-recommended-product-dependencies/src/test/groovy/com/palantir/gradle/dist/RecommendedProductDependenciesPluginIntegrationSpec.groovy
@@ -27,7 +27,7 @@ class RecommendedProductDependenciesPluginIntegrationSpec extends IntegrationSpe
         System.setProperty("ignoreDeprecations", "true")
         buildFile << """
         plugins {
-            id 'com.palantir.consistent-versions' version '2.25.0' apply false
+            id 'com.palantir.consistent-versions' version '3.3.0' apply false
         }
         apply plugin: 'java'
         apply plugin: 'com.palantir.recommended-product-dependencies'
