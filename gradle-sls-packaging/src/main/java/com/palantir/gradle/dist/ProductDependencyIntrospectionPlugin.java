2ca0f1b9fadc079826b4f6fc6c5de592bed0d00d
Finlay Williams <finlayw@palantir.com>
2026-01-26 10:41:40 +0000

    tidy

--- gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/ProductDependencyIntrospectionPlugin.java
+++ gradle-sls-packaging/src/main/java/com/palantir/gradle/dist/ProductDependencyIntrospectionPlugin.java
@@ -129,9 +129,6 @@ public abstract class ProductDependencyIntrospectionPlugin implements Plugin<Pro
     }
 
     public static Map<ProductId, Project> getInRepoProductIds(Project rootProject) {
-        // Use path comparison instead of identity check (==) for Gradle 9 compatibility.
-        // During dependency resolution, Gradle may wrap/decorate project instances,
-        // causing identity comparison to fail even for the actual root project.
         Preconditions.checkArgument(
                 rootProject.getPath().equals(rootProject.getRootProject().getPath()),
                 "Must call this method with the root project",
